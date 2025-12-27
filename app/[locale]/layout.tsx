e9183efbd6dc5e8703ae5c3e632e1c88204b369d
modabucksmain-pixel <modabucks.main@gmail.com>
2025-12-27 12:16:50 +0300

    Revert "Integrate Google AdSense script into website's head section for ad readiness"

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,4 +1,3 @@
-import Script from 'next/script';
 import { Header } from '@/components/header';
 import { Footer } from '@/components/footer';
 import { getTranslations, getDirection, type Locale } from '@/lib/i18n';
@@ -17,14 +16,6 @@ export default async function LocaleLayout({
 
   return (
     <html lang={locale} dir={direction}>
-      <head>
-        <Script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1785692102378858"
-          crossOrigin="anonymous"
-          strategy="afterInteractive"
-        />
-      </head>
       <body className="min-h-screen bg-white">
         <Header locale={locale} translations={translations} />
         <main className="flex-1">
